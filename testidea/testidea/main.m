9a202035a9b21968e2b4c851211e23e6e51676d5
Tonya <TonyaMSweeting@gmail.com>
2012-02-25 16:31:00 -0500

    testing

---
+++ testidea/testidea/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  testidea
+//
+//  Created by Me on 2/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "testideaAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([testideaAppDelegate class]));
+    }
+}
---
+++ testidea/testidea/testideaAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  testideaAppDelegate.h
+//  testidea
+//
+//  Created by Me on 2/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class testideaViewController;
+
+@interface testideaAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) testideaViewController *viewController;
+
+@end
